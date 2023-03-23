071821e19b043c1e424eeca14957ca15d80b39ab
eliyahugez <eliyahugez@gmail.com>
2023-03-23 19:00:03 +0200

    סדג

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,17 +1,22 @@
 import { Divider, Typography } from "@mui/material";
-
+import { string } from 'prop-types';
 const PageHeader = ({ title, subtitle }) => {
-  return (
-    <>
-      <Typography variant="h2" component="h1">
-        {title}
-      </Typography>
-      <Typography variant="h5" component="h2">
-        {subtitle}
-      </Typography>
-      <Divider sx={{ my: 2 }} />
-    </>
-  );
+    return (
+        <>
+            <Typography variant="h2" component="h1">
+                {title}
+            </Typography>
+            <Typography variant="h5" component="h2">
+                {subtitle}
+            </Typography>
+            <Divider sx={{ my: 2 }} />
+        </>
+    );
+};
+
+PageHeader.prototype = {
+    title: string.isRequired,
+    subtitle: string.isRequired,
 };
 
 export default PageHeader;
