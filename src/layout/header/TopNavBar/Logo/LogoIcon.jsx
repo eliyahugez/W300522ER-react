23b9ca3dfac03afe38e6004ccc6febaaf3932dfc
Victor Yampolsky <tsanhan@gmail.com>
2023-03-30 19:59:06 +0300

    logo icon

---
+++ src/layout/header/TopNavBar/Logo/LogoIcon.jsx
@@ -0,0 +1,29 @@
+import { Avatar, IconButton } from "@mui/material";
+import NavBarLink from "../../../../routes/NavBarLink";
+import ROUTES from "../../../../routes/routesModel";
+
+const LogoIcon = () => {
+  return (
+    <NavBarLink to={ROUTES.CARDS}>
+      <IconButton
+        sx={{
+          display: {
+            xs: "inline-flex",
+            md: "none",
+          },
+        }}
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+      >
+        <Avatar 
+            alt="Business Card Icon"
+            src="/assets/images/business-card.png"
+        />
+      </IconButton>
+    </NavBarLink>
+  );
+};
+
+export default LogoIcon;
