ce99b2ebb9bb8c63fcb7fac13b89a5c14b04cfe4
VICTOR YAMPOLSKT <VICTOR.YAMPOLSKY@RETAILIN.CO>
2023-04-13 21:34:24 +0300

    axios start

---
+++ src/cards/services/cardApiService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:8181';
+
+export const getCards = async () => {
+    try {
+        const { data } = await axios.get(`${apiUrl}/cards`);
+        return data;
+    } catch (error) {
+        return Promise.reject(error.message);
+    }
+}
