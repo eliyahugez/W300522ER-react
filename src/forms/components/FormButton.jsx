601fafcb4d7b98d45b8e03a780c38e2da84bc3e5
Victor Yampolsky <tsanhan@gmail.com>
2023-05-22 19:57:13 +0300

    FormButton

---
+++ src/forms/components/FormButton.jsx
@@ -0,0 +1,20 @@
+import { Button } from "@mui/material"
+
+const FormButton = ({
+    variant,
+    component,
+    size,
+    color,
+    onClick,
+    disabled,
+    node
+}) => {
+    return (
+        <Button
+            variant={variant}
+        >
+
+        </Button>
+
+    )
+}
