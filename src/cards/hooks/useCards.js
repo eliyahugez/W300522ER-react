95dbbb745b7cd22ad6bfad65c46d856a4e70984b
eliyahugez <121052211+eliyahugez@users.noreply.github.com>
2023-06-27 23:01:20 +0300

    27.6

--- src/cards/hooks/useCards.js
+++ src/cards/hooks/useCards.js
@@ -7,6 +7,8 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import ROUTES from "../../routes/routesModel";
 import { useUser } from "../../users/providers/UserProvider";
 
+
+
 const useCards = () => {
     const [cards, setCards] = useState(null);
     const [card, setCard] = useState(null);
